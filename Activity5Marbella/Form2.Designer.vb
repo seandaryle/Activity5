9c5815200f87c62f687960e1c6bc3437a3a34098
seandaryle <129166696+seandaryle@users.noreply.github.com>
2023-04-22 11:54:24 +0800

    Add files via upload

---
+++ Activity5Marbella/Form2.Designer.vb
@@ -0,0 +1,263 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form2))
+        Me.UploadCsvButton = New System.Windows.Forms.Button()
+        Me.BackupCsvButton = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.CurrentCellTextBox = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ColumnTextBox = New System.Windows.Forms.TextBox()
+        Me.RowTextBox = New System.Windows.Forms.TextBox()
+        Me.RemoveRowButton = New System.Windows.Forms.Button()
+        Me.GoToCellButton = New System.Windows.Forms.Button()
+        Me.ClearGridButton = New System.Windows.Forms.Button()
+        Me.DeleteCsvButton = New System.Windows.Forms.Button()
+        Me.RemoveColumnButton = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'UploadCsvButton
+        '
+        Me.UploadCsvButton.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.UploadCsvButton.BackgroundImage = CType(resources.GetObject("UploadCsvButton.BackgroundImage"), System.Drawing.Image)
+        Me.UploadCsvButton.FlatAppearance.BorderSize = 0
+        Me.UploadCsvButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.UploadCsvButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.UploadCsvButton.ForeColor = System.Drawing.Color.FloralWhite
+        Me.UploadCsvButton.Location = New System.Drawing.Point(405, 38)
+        Me.UploadCsvButton.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.UploadCsvButton.Name = "UploadCsvButton"
+        Me.UploadCsvButton.Size = New System.Drawing.Size(191, 55)
+        Me.UploadCsvButton.TabIndex = 1
+        Me.UploadCsvButton.Text = "Upload CSV File"
+        Me.UploadCsvButton.UseVisualStyleBackColor = False
+        '
+        'BackupCsvButton
+        '
+        Me.BackupCsvButton.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.BackupCsvButton.BackgroundImage = CType(resources.GetObject("BackupCsvButton.BackgroundImage"), System.Drawing.Image)
+        Me.BackupCsvButton.FlatAppearance.BorderSize = 0
+        Me.BackupCsvButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BackupCsvButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BackupCsvButton.ForeColor = System.Drawing.Color.FloralWhite
+        Me.BackupCsvButton.Location = New System.Drawing.Point(878, 38)
+        Me.BackupCsvButton.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.BackupCsvButton.Name = "BackupCsvButton"
+        Me.BackupCsvButton.Size = New System.Drawing.Size(199, 55)
+        Me.BackupCsvButton.TabIndex = 2
+        Me.BackupCsvButton.Text = "Create a Back-up"
+        Me.BackupCsvButton.UseVisualStyleBackColor = False
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.BackgroundColor = System.Drawing.Color.Gainsboro
+        Me.DataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.DataGridView1.Location = New System.Drawing.Point(405, 122)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(672, 359)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'CurrentCellTextBox
+        '
+        Me.CurrentCellTextBox.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.CurrentCellTextBox.Location = New System.Drawing.Point(405, 486)
+        Me.CurrentCellTextBox.Name = "CurrentCellTextBox"
+        Me.CurrentCellTextBox.Size = New System.Drawing.Size(672, 22)
+        Me.CurrentCellTextBox.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Bisque
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label2.Location = New System.Drawing.Point(496, 535)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(116, 20)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Enter Column:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Bisque
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label1.Location = New System.Drawing.Point(794, 539)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(92, 20)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Enter Row:"
+        '
+        'ColumnTextBox
+        '
+        Me.ColumnTextBox.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.ColumnTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.ColumnTextBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ColumnTextBox.ForeColor = System.Drawing.Color.Black
+        Me.ColumnTextBox.Location = New System.Drawing.Point(618, 535)
+        Me.ColumnTextBox.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.ColumnTextBox.Name = "ColumnTextBox"
+        Me.ColumnTextBox.Size = New System.Drawing.Size(127, 23)
+        Me.ColumnTextBox.TabIndex = 12
+        Me.ColumnTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'RowTextBox
+        '
+        Me.RowTextBox.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.RowTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RowTextBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RowTextBox.ForeColor = System.Drawing.Color.Black
+        Me.RowTextBox.Location = New System.Drawing.Point(892, 539)
+        Me.RowTextBox.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.RowTextBox.Name = "RowTextBox"
+        Me.RowTextBox.Size = New System.Drawing.Size(127, 23)
+        Me.RowTextBox.TabIndex = 13
+        Me.RowTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'RemoveRowButton
+        '
+        Me.RemoveRowButton.BackColor = System.Drawing.Color.Gold
+        Me.RemoveRowButton.FlatAppearance.BorderSize = 0
+        Me.RemoveRowButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.RemoveRowButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RemoveRowButton.ForeColor = System.Drawing.Color.Black
+        Me.RemoveRowButton.Location = New System.Drawing.Point(115, 292)
+        Me.RemoveRowButton.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.RemoveRowButton.Name = "RemoveRowButton"
+        Me.RemoveRowButton.Size = New System.Drawing.Size(167, 55)
+        Me.RemoveRowButton.TabIndex = 22
+        Me.RemoveRowButton.Text = "Remove Row"
+        Me.RemoveRowButton.UseVisualStyleBackColor = False
+        '
+        'GoToCellButton
+        '
+        Me.GoToCellButton.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.GoToCellButton.FlatAppearance.BorderSize = 0
+        Me.GoToCellButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.GoToCellButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GoToCellButton.ForeColor = System.Drawing.Color.Black
+        Me.GoToCellButton.Location = New System.Drawing.Point(48, 144)
+        Me.GoToCellButton.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.GoToCellButton.Name = "GoToCellButton"
+        Me.GoToCellButton.Size = New System.Drawing.Size(308, 32)
+        Me.GoToCellButton.TabIndex = 21
+        Me.GoToCellButton.Text = "Search"
+        Me.GoToCellButton.UseVisualStyleBackColor = False
+        '
+        'ClearGridButton
+        '
+        Me.ClearGridButton.BackColor = System.Drawing.Color.Gold
+        Me.ClearGridButton.FlatAppearance.BorderSize = 0
+        Me.ClearGridButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ClearGridButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ClearGridButton.ForeColor = System.Drawing.Color.Black
+        Me.ClearGridButton.Location = New System.Drawing.Point(115, 213)
+        Me.ClearGridButton.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.ClearGridButton.Name = "ClearGridButton"
+        Me.ClearGridButton.Size = New System.Drawing.Size(167, 55)
+        Me.ClearGridButton.TabIndex = 20
+        Me.ClearGridButton.Text = "Clear all"
+        Me.ClearGridButton.UseVisualStyleBackColor = False
+        '
+        'DeleteCsvButton
+        '
+        Me.DeleteCsvButton.BackColor = System.Drawing.Color.Gold
+        Me.DeleteCsvButton.FlatAppearance.BorderSize = 0
+        Me.DeleteCsvButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.DeleteCsvButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DeleteCsvButton.ForeColor = System.Drawing.Color.Black
+        Me.DeleteCsvButton.Location = New System.Drawing.Point(115, 453)
+        Me.DeleteCsvButton.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.DeleteCsvButton.Name = "DeleteCsvButton"
+        Me.DeleteCsvButton.Size = New System.Drawing.Size(167, 55)
+        Me.DeleteCsvButton.TabIndex = 19
+        Me.DeleteCsvButton.Text = "Delete"
+        Me.DeleteCsvButton.UseVisualStyleBackColor = False
+        '
+        'RemoveColumnButton
+        '
+        Me.RemoveColumnButton.BackColor = System.Drawing.Color.Gold
+        Me.RemoveColumnButton.FlatAppearance.BorderSize = 0
+        Me.RemoveColumnButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.RemoveColumnButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RemoveColumnButton.ForeColor = System.Drawing.Color.Black
+        Me.RemoveColumnButton.Location = New System.Drawing.Point(115, 371)
+        Me.RemoveColumnButton.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.RemoveColumnButton.Name = "RemoveColumnButton"
+        Me.RemoveColumnButton.Size = New System.Drawing.Size(167, 55)
+        Me.RemoveColumnButton.TabIndex = 23
+        Me.RemoveColumnButton.Text = "Remove Column"
+        Me.RemoveColumnButton.UseVisualStyleBackColor = False
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Salmon
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(1108, 669)
+        Me.Controls.Add(Me.RemoveColumnButton)
+        Me.Controls.Add(Me.RemoveRowButton)
+        Me.Controls.Add(Me.GoToCellButton)
+        Me.Controls.Add(Me.ClearGridButton)
+        Me.Controls.Add(Me.DeleteCsvButton)
+        Me.Controls.Add(Me.RowTextBox)
+        Me.Controls.Add(Me.ColumnTextBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.CurrentCellTextBox)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.BackupCsvButton)
+        Me.Controls.Add(Me.UploadCsvButton)
+        Me.ForeColor = System.Drawing.Color.Azure
+        Me.Name = "Form2"
+        Me.Text = "Activity 5: Project Data Loading and Conversion plus Database Backup Functions"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents UploadCsvButton As Button
+    Friend WithEvents BackupCsvButton As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents CurrentCellTextBox As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ColumnTextBox As TextBox
+    Friend WithEvents RowTextBox As TextBox
+    Friend WithEvents RemoveRowButton As Button
+    Friend WithEvents GoToCellButton As Button
+    Friend WithEvents ClearGridButton As Button
+    Friend WithEvents DeleteCsvButton As Button
+    Friend WithEvents RemoveColumnButton As Button
+End Class
---
+++ Activity5Marbella/Form2.vb
@@ -0,0 +1,301 @@
+﻿Imports System.ComponentModel
+Imports System.IO
+Imports System.Text
+
+Public Class Form2
+
+    Private Sub LoadCsvData(sender As Object, e As RunWorkerCompletedEventArgs)
+        Dim csvData As List(Of String()) = DirectCast(e.Result, List(Of String()))
+        Dim header As String() = csvData(0)
+
+
+        Dim dt As New DataTable()
+        For Each colName As String In header
+            dt.Columns.Add(New DataColumn(colName, GetType(String)))
+        Next
+
+
+        For i As Integer = 1 To csvData.Count - 1
+            Dim fields As String() = csvData(i)
+            Dim row As DataRow = dt.NewRow()
+            row.ItemArray = fields
+            dt.Rows.Add(row)
+        Next
+
+
+        DataGridView1.DataSource = dt
+    End Sub
+
+    Private Sub UploadCsvButton_Click(sender As Object, e As EventArgs) Handles UploadCsvButton.Click
+
+        Dim openFileDialog1 As New OpenFileDialog()
+        openFileDialog1.Filter = "CSV Files (*.csv)|*.csv|All Files (*.*)|*.*"
+
+
+        If openFileDialog1.ShowDialog() = DialogResult.OK Then
+            Dim fileName As String = openFileDialog1.FileName
+            Dim bgWorker As New BackgroundWorker()
+            AddHandler bgWorker.DoWork, AddressOf ParseCsvFile
+            AddHandler bgWorker.RunWorkerCompleted, AddressOf LoadCsvData
+            bgWorker.RunWorkerAsync(fileName)
+        End If
+    End Sub
+
+    Private Sub ParseCsvFile(sender As Object, e As DoWorkEventArgs)
+
+        Dim fileName As String = DirectCast(e.Argument, String)
+        Dim csvData As New List(Of String())
+        Using reader As New StreamReader(fileName)
+            While Not reader.EndOfStream
+
+                Dim line As String = reader.ReadLine()
+                Dim fields As String() = line.Split(","c)
+
+                csvData.Add(fields)
+            End While
+        End Using
+        e.Result = csvData
+    End Sub
+
+    Private Sub BackupCsvButton_Click(sender As Object, e As EventArgs) Handles BackupCsvButton.Click
+
+        If DataGridView1.Rows.Count = 0 Then
+
+            Dim result As DialogResult = MessageBox.Show("Error: DataGridView is empty. Do you still want to create a backup?", "Backup Empty DataGridView", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+            If result = DialogResult.No Then
+                Return
+            End If
+        End If
+
+
+        Dim saveFileDialog1 As New SaveFileDialog()
+
+        saveFileDialog1.Filter = "CSV Files (.csv)|.csv|All Files (.)|."
+        If saveFileDialog1.ShowDialog() = DialogResult.OK Then
+
+            Dim fileName As String = saveFileDialog1.FileName
+            Using writer As New StreamWriter(fileName)
+
+                Dim headers As New List(Of String)
+
+                For Each column As DataGridViewColumn In DataGridView1.Columns
+                    headers.Add(column.HeaderText)
+                Next
+
+                writer.WriteLine(String.Join(",", headers))
+
+                For Each row As DataGridViewRow In DataGridView1.Rows
+                    Dim fields As New StringBuilder()
+                    For Each cell As DataGridViewCell In row.Cells
+
+                        If Not String.IsNullOrWhiteSpace(cell.Value?.ToString()) Then
+                            fields.Append(cell.Value.ToString())
+                        End If
+
+                        fields.Append(",")
+                    Next
+
+                    writer.WriteLine(fields.ToString().TrimEnd(","))
+                Next
+            End Using
+            MessageBox.Show("Your Backup was successfully created!")
+        End If
+    End Sub
+
+
+    Private openFileDialog1 As New OpenFileDialog()
+
+
+    Private Const FILE_FILTER As String = "CSV Files (*.csv)|*.csv|All Files (*.*)|*.*"
+    Private Const FILE_SELECT_TITLE As String = "Select a CSV file to delete"
+
+
+    Private Sub DeleteCsvButton_Click(sender As Object, e As EventArgs) Handles DeleteCsvButton.Click
+
+        openFileDialog1.Filter = FILE_FILTER
+        openFileDialog1.Title = FILE_SELECT_TITLE
+        openFileDialog1.Multiselect = False
+
+
+        If openFileDialog1.ShowDialog() <> DialogResult.OK Then
+            Return
+        End If
+
+
+        Dim filePath As String = openFileDialog1.FileName
+        Dim fileName As String = Path.GetFileName(filePath)
+
+
+        Dim confirmResult = MessageBox.Show($"Are you sure you want to delete the file {fileName}?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2)
+
+
+        If confirmResult <> DialogResult.Yes Then
+            Return
+        End If
+
+
+        Try
+            File.Delete(filePath)
+            MessageBox.Show("File was Deleted", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As IOException
+            MessageBox.Show($"Error: A problem occurred while deleting the file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Catch ex As UnauthorizedAccessException
+            MessageBox.Show($"Error: Permission Denied: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+
+    Private Sub DataGridView1_CurrentCellChanged(sender As Object, e As EventArgs) Handles DataGridView1.CurrentCellChanged
+
+        Dim currentCell As DataGridViewCell = DataGridView1.CurrentCell
+
+
+        If currentCell?.RowIndex IsNot Nothing AndAlso currentCell?.ColumnIndex IsNot Nothing Then
+
+            Dim row As String = (currentCell.RowIndex + 1).ToString("D2")
+            Dim column As String = (currentCell.ColumnIndex + 1).ToString("D2")
+
+
+            CurrentCellTextBox.Text = $"Column {column}     Row {row}"
+        Else
+
+            CurrentCellTextBox.Text = ""
+        End If
+    End Sub
+
+
+    Private Sub ClearGridButton_Click(sender As Object, e As EventArgs) Handles ClearGridButton.Click
+
+
+        If DataGridView1.Rows.Count = 0 AndAlso DataGridView1.Columns.Count = 0 Then
+
+            MessageBox.Show("Data Grid View is empty.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Return
+        End If
+
+
+        Dim rowCount As Integer = DataGridView1.Rows.Count
+        Dim columnCount As Integer = DataGridView1.Columns.Count
+
+
+        Dim message As String = $"Are you sure you want to clear the DataGridView?{vbCrLf}This will delete {rowCount} rows and {columnCount} columns."
+
+
+        Dim result = MessageBox.Show(message, "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+
+        If result = DialogResult.Yes Then
+            DataGridView1.DataSource = Nothing
+        End If
+
+    End Sub
+
+    Private Sub GoToCellButton_Click(sender As Object, e As EventArgs) Handles GoToCellButton.Click
+
+        If DataGridView1.RowCount = 0 OrElse DataGridView1.ColumnCount = 0 OrElse String.IsNullOrEmpty(RowTextBox.Text) OrElse String.IsNullOrEmpty(ColumnTextBox.Text) Then
+
+            RowTextBox.Text = " "
+            ColumnTextBox.Text = " "
+            Return
+        End If
+
+
+        Dim row As Integer = CInt(RowTextBox.Text)
+        Dim column As Integer = CInt(ColumnTextBox.Text)
+
+
+        If row > 0 AndAlso row <= DataGridView1.RowCount AndAlso column > 0 AndAlso column <= DataGridView1.ColumnCount Then
+
+            DataGridView1.CurrentCell = DataGridView1(column - 1, row - 1)
+            DataGridView1.FirstDisplayedScrollingRowIndex = row - 1
+        Else
+            MessageBox.Show("Invalid row or column number.")
+        End If
+    End Sub
+
+    Private Function IsIndexSpecified(indexTextBox As TextBox) As Boolean
+
+        If String.IsNullOrEmpty(indexTextBox.Text) Then
+            MessageBox.Show("Oops Incorrect index number.")
+            Return False
+        End If
+        Return True
+    End Function
+
+    Private Sub RemoveRowButton_Click(sender As Object, e As EventArgs) Handles RemoveRowButton.Click
+
+        Dim rowNumber As Integer
+        If String.IsNullOrEmpty(RowTextBox.Text) OrElse Not Integer.TryParse(RowTextBox.Text, rowNumber) Then
+            MessageBox.Show("Invalid Row number")
+            Return
+        End If
+
+        If DataGridView1.ColumnHeadersVisible Then
+            rowNumber -= 1
+        End If
+
+
+        If rowNumber < 0 OrElse rowNumber >= DataGridView1.Rows.Count Then
+            MessageBox.Show($"Row number {rowNumber + 1} does not exist")
+            Return
+        End If
+
+
+        Dim result = MessageBox.Show($"Are you sure you want to remove row number {rowNumber + 1}?", "Confirm", MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+
+            DataGridView1.Rows.RemoveAt(rowNumber)
+        End If
+    End Sub
+
+    Private Sub RemoveColumnButton_Click(sender As Object, e As EventArgs) Handles RemoveColumnButton.Click
+
+        Dim columnNumber As Integer
+        If Not IsIndexSpecified(ColumnTextBox) OrElse String.IsNullOrEmpty(ColumnTextBox.Text) OrElse Not Integer.TryParse(ColumnTextBox.Text, columnNumber) Then
+            MessageBox.Show("Invalid Column number")
+            Return
+        End If
+        columnNumber -= 1
+
+
+        If columnNumber < 0 OrElse columnNumber >= DataGridView1.Columns.Count Then
+            MessageBox.Show($"Column number {columnNumber + 1} does not exist")
+            Return
+        End If
+
+
+        Dim result = MessageBox.Show($"Are you sure you want to remove column number {columnNumber + 1}?", "Confirm", MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+
+            DataGridView1.Columns.RemoveAt(columnNumber)
+        End If
+    End Sub
+
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        If e.RowIndex >= 0 AndAlso e.ColumnIndex >= 0 Then
+
+            RowTextBox.Text = $"{e.RowIndex + 1}"
+            ColumnTextBox.Text = $"{e.ColumnIndex + 1}"
+        Else
+
+            RowTextBox.Clear()
+            ColumnTextBox.Clear()
+        End If
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub CurrentCellTextBox_TextChanged(sender As Object, e As EventArgs) Handles CurrentCellTextBox.TextChanged
+
+    End Sub
+
+    Private Sub ColumnTextBox_TextChanged(sender As Object, e As EventArgs) Handles ColumnTextBox.TextChanged
+
+    End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
